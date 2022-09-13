87c91f29a8e6b1f0f20cad14fb4e084fc5d5b6b1
Amirhossein Payab <89769597+amirhosseinpayab@users.noreply.github.com>
2022-09-13 14:03:25 +0430

    Add files via upload

---
+++ Guess Gold Number.py
@@ -0,0 +1,19 @@
+import random
+guess = 0
+print('Hello\nwelcome to Guess Gold Number game\nYou have three chance to guess :)')
+number = int(input("Game is from 1 to : "))
+gold_number = random.randint(1,number)
+for i in range(3):
+    guess = int(input("Guess the gold number : "))
+    if guess == gold_number:
+        print(f"You fond the gold number :)))))))))) {gold_number}")
+        break
+    elif guess < gold_number:
+        print("It is lower than gold number !")
+    elif guess > gold_number:
+        print("It is highter than gold number !")
+    
+    
+
+
+
